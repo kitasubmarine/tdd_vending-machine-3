37ce6e1f8dbc8852b79759481fe4d1416a6c2073
RyoKobayashi0228 <kbry0919@gmail.com>
2020-08-06 13:55:34 +0900

    第2回work shop

--- tests/test_vending_machine.py
+++ tests/test_vending_machine.py
@@ -1,8 +1,27 @@
 from vending_machine.vending_machine import vending_machine
+# リファクタリングの余地あり
 
 class test_vending_machine:
     def test_insert(self):
         vdd = vending_machine()
-        yen = 1
-        assert (vdd.insert(yen) == 10) or (vdd.insert(yen) == 50) /
-        or (vdd.insert(yen) == 100) or (vdd.insert(yen) == 500) or (vdd.insert(yen) == 1000)
+        # pytest parameterizeを使ってスマートに書く
+        # 1つの関数で複数のテストが出来るようにする
+        for yen in (10, 50, 100, 500, 1000):
+            assert vdd.insert(yen)
+        for yen in (1, 5, 2000, 5000, 10000):
+            assert ~vdd.insert(yen)
+
+    def test_get_total(self):
+        vdd = vending_machine()
+        for yen in (10, 50, 100, 500, 1000):
+            vdd.insert(yen)
+        assert vdd.get_total() == 1660
+
+    def test_reject_money(self):
+        vdd = vending_machine()
+        for yen in (10, 50, 100, 500, 1000):
+            vdd.insert(yen)
+        assert vdd.reject_money() == 1660
+        assert vdd.money == 0
+
+        
--- vending_machine/vending_machine.py
+++ vending_machine/vending_machine.py
@@ -1,20 +1,31 @@
 class vending_machine:
+ # リファクタリングの余地あり
+
+#  coinクラスを作って、40円コインがないようにする
+    price={'コーラ': 120}
     def __init__(self):
         self.money=0
+        self.storage={'コーラ': 5}
+
 
     def insert(self,yen):
-        available_yen = [1,5,10,50,100,500,1000]
+        available_yen = [10,50,100,500,1000]
         if yen in available_yen:
             self.money += yen
-            return yen
+            return True
         else:
-            return yen
+            return False
 
-    def get_total(self,yen):
+    def get_total(self):
         return self.money
     
     def reject_money(self):
         tmp=self.money
         self.money=0
         return tmp
+
+    def buy(self, drink_name):
+        for item in self.storage.keys():
+            if self.storage[item] >= 1:
+                self.storage[item] -= 1
 
